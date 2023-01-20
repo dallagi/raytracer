be729781ccbe65f81dd4fd6c5e092b3353ceb53e
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-01-20 19:48:53 +0100

    Add matrix cofactor

--- src/matrix.rs
+++ src/matrix.rs
@@ -62,6 +62,16 @@ impl Matrix<3, 3> {
     pub fn minor(self, row: usize, column: usize) -> f32 {
         self.submatrix(row, column).determinant()
     }
+
+    pub fn cofactor(self, row: usize, column: usize) -> f32 {
+        let res = self.minor(row, column);
+
+        if (row + column) % 2 == 0 {
+            res
+        } else {
+            -res
+        }
+    }
 }
 
 impl<const ROWS: usize, const COLS: usize> ops::Index<(usize, usize)> for Matrix<ROWS, COLS> {
@@ -304,5 +314,15 @@ mod tests {
 
         assert_eq!(25.0, sub.determinant());
         assert_eq!(25.0, matrix.minor(1, 0));
+    }
+
+    #[test]
+    fn cofactor_of_3x3_matrix() {
+        let matrix = Matrix::new([[3.0, 5.0, 0.0], [2.0, -1.0, -7.0], [6.0, -1.0, 5.0]]);
+
+        assert_eq!(-12.0, matrix.minor(0, 0));
+        assert_eq!(-12.0, matrix.cofactor(0, 0));
+        assert_eq!(25.0, matrix.minor(1, 0));
+        assert_eq!(-25.0, matrix.cofactor(1, 0));
     }
 }
