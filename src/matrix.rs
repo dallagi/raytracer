b6934c7792d926425fef9cd2d02d3d407922dabf
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-01-20 21:24:47 +0100

    Add more tests for matrix inversion

--- src/matrix.rs
+++ src/matrix.rs
@@ -439,4 +439,40 @@ mod tests {
         assert!((-160.0 / 532.0).float_eq(expected_inverse[(3, 2)]));
         assert_eq!(expected_inverse, matrix.inverse());
     }
+
+    #[test]
+    fn inverse_of_another_matrix() {
+        let matrix = Matrix::new([
+            [8.0, -5.0, 9.0, 2.0],
+            [7.0, 5.0, 6.0, 1.0],
+            [-6.0, 0.0, 9.0, 6.0],
+            [-3.0, 0.0, -9.0, -4.0],
+        ]);
+
+        let expected_inverse = Matrix::new([
+            [-0.15385, -0.15385, -0.28205, -0.53846],
+            [-0.07692, 0.12308, 0.02564, 0.03077],
+            [0.35897, 0.35897, 0.43590, 0.92308],
+            [-0.69231, -0.69231, -0.76923, -1.92308],
+        ]);
+        assert_eq!(expected_inverse, matrix.inverse())
+    }
+
+    #[test]
+    fn inverse_of_a_third_matrix() {
+        let matrix = Matrix::new([
+            [9.0, 3.0, 0.0, 9.0],
+            [-5.0, -2.0, -6.0, -3.0],
+            [-4.0, 9.0, 6.0, 4.0],
+            [-7.0, 6.0, 6.0, 2.0],
+        ]);
+
+        let expected_inverse = Matrix::new([
+            [-0.04074, -0.07778, 0.14444, -0.22222],
+            [-0.07778, 0.03333, 0.36667, -0.33333],
+            [-0.02901, -0.14630, -0.10926, 0.12963],
+            [0.17778, 0.06667, -0.26667, 0.33333],
+        ]);
+        assert_eq!(expected_inverse, matrix.inverse())
+    }
 }
