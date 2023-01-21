85fec6267bd388f2ba86276a5fd80d1cebf05ae2
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-01-21 10:03:00 +0100

    Add rotation around z axis

--- src/matrix/transformations.rs
+++ src/matrix/transformations.rs
@@ -41,6 +41,16 @@ pub fn rotation_y(radians: f32) -> Matrix<4, 4> {
 
     result
 }
+pub fn rotation_z(radians: f32) -> Matrix<4, 4> {
+    let mut result: Matrix<4, 4> = Matrix::identity();
+
+    result[(0, 0)] = radians.cos();
+    result[(0, 1)] = -radians.sin();
+    result[(1, 0)] = radians.sin();
+    result[(1, 1)] = radians.cos();
+
+    result
+}
 
 #[cfg(test)]
 mod tests {
@@ -146,5 +156,19 @@ mod tests {
             half_quarter * point
         );
         assert_eq!(Point::new(1.0, 0.0, 0.0), full_quarter * point);
+    }
+
+    #[test]
+    fn rotation_z_rotates_a_point_around_z_axis() {
+        let point = Point::new(0.0, 1.0, 0.0);
+
+        let half_quarter = rotation_z(PI / 4.0);
+        let full_quarter = rotation_z(PI / 2.0);
+
+        assert_eq!(
+            Point::new(-(2.0_f32).sqrt() / 2.0, (2.0_f32).sqrt() / 2.0, 0.0),
+            half_quarter * point
+        );
+        assert_eq!(Point::new(-1.0, 0.0, 0.0), full_quarter * point);
     }
 }
