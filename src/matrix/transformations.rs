c4557a410c8fd86378d49c615d8f9e3a8f48fed4
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-01-21 09:29:00 +0100

    Implement scaling

--- src/matrix/transformations.rs
+++ src/matrix/transformations.rs
@@ -10,6 +10,16 @@ pub fn translation(x: f32, y: f32, z: f32) -> Matrix<4, 4> {
     result
 }
 
+pub fn scaling(x: f32, y: f32, z: f32) -> Matrix<4, 4> {
+    let mut result: Matrix<4, 4> = Matrix::identity();
+
+    result[(0, 0)] = x;
+    result[(1, 1)] = y;
+    result[(2, 2)] = z;
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{point::Point, vector::Vector};
@@ -17,7 +27,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn multiplying_by_translation_matrix() {
+    fn translation_moves_a_point() {
         let transform = translation(5.0, -3.0, 2.0);
         let point = Point::new(-3.0, 4.0, 5.0);
 
@@ -38,5 +48,29 @@ mod tests {
         let vector = Vector::new(-3.0, 4.0, 5.0);
 
         assert_eq!(vector, transform * vector);
+    }
+
+    #[test]
+    fn scaling_moves_a_point_by_multiplication() {
+        let transform = scaling(2.0, 3.0, 4.0);
+        let point = Point::new(-4.0, 6.0, 8.0);
+
+        assert_eq!(Point::new(-8.0, 18.0, 32.0), transform * point);
+    }
+
+    #[test]
+    fn scaling_changes_length_of_vector() {
+        let transform = scaling(2.0, 3.0, 4.0);
+        let vector = Vector::new(-4.0, 6.0, 8.0);
+
+        assert_eq!(Vector::new(-8.0, 18.0, 32.0), transform * vector);
+    }
+
+    #[test]
+    fn inverse_of_scaling_shrinks_a_vector() {
+        let transform = scaling(2.0, 3.0, 4.0).inverse();
+        let vector = Vector::new(-4.0, 6.0, 8.0);
+
+        assert_eq!(Vector::new(-2.0, 2.0, 2.0), transform * vector);
     }
 }
